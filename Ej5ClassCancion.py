d2d36ff9d458188f8dd933813012560a0acf32ca
lsarappa <sarappalucas2000@gmail.com>
2018-04-25 17:38:06 -0300

    SALAH

---
+++ Ej5ClassCancion.py
@@ -0,0 +1,9 @@
+class Cancion (object):
+
+    titulo = None
+    Lista_Artistas =[]
+    Lista_Autores = []
+
+    def __init__ (self,titulo):
+
+        self.titulo = titulo
---
+++ Ej5ClassPersonas.py
@@ -0,0 +1,24 @@
+class Persona (object):
+
+    nombre = None
+    apellido = None
+    fecha_nacimiento = None
+
+    def __init__(self,nombre,apellido,fecha_de_nacimiento):
+
+        self.nombre = nombre
+        self.apellido=apellido
+        self.fecha_nacimiento = fecha_de_nacimiento
+
+class Artistas (Persona):
+
+    pass
+
+class Autores (Persona):
+
+    nacionalidad = None
+
+    def AgregarNacionalidad (self, nacionalidad):
+
+        self.nacionalidad = nacionalidad
+
