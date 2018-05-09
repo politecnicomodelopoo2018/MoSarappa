9a26c53141de8d63822d3f4eadc4733850c31e16
lsarappa <sarappalucas2000@gmail.com>
2018-05-09 17:36:21 -0300

    GHY

--- Ej6ClassEmpresa.py
+++ Ej6ClassEmpresa.py
@@ -4,8 +4,9 @@ class empresa(object):
 
     nombre = None
 
-    def __init__(self):
+    def __init__(self, nombre):
 
+        self.nombre = nombre
         self.Lista_autos = []
         self.Lista_camioneta = []
 
@@ -17,3 +18,43 @@ class empresa(object):
     def AgregarCamioneta(self, unaCamioneta):
 
         self.Lista_camioneta.append(unaCamioneta)
+
+    def ListaPatentesAParaMostrar (self):
+        listaPatenteA = []
+        for item in self.Lista_autos:
+            listaPatenteA.append(item.patente)
+        return listaPatenteA
+
+    def ListaPatentesCParaMostrar (self):
+        listaPatenteC = []
+        for item in self.Lista_camioneta:
+            listaPatenteC.append(item.patente)
+        return listaPatenteC
+
+    def abrirPA(self):
+        with open("PatentesA.txt", "w") as f:
+
+            for item in self.ListaPatentesAParaMostrar():
+                f.write(item + '\n')
+
+    def abrirPC(self):
+
+
+        with open("PatentesC.txt", "w") as f:
+
+            for item in self.ListaPatentesCParaMostrar():
+                f.write (item + '\n')
+
+    def PatentesA (self):
+
+        with open("PatentesA.txt", "r") as f:
+
+            for line in f:
+                print(line)
+
+    def PatentesC (self):
+
+        with open("PatentesC.txt", "r") as f:
+
+            for line in f:
+                print(line)                  
--- Ej6ClassVehiculo.py
+++ Ej6ClassVehiculo.py
@@ -2,19 +2,35 @@ class vehiculo (object):
 
     patente = None
     cant_ruedas = None
-    año_fab = None
+    ano_fab = None
 
-    def __init__(self,patente,cant_ruedas,año_fab):
+    def __init__(self,patente,cant_ruedas,ano_fab):
 
         self.patente = patente
         self.cant_ruedas = cant_ruedas
-        self.año_fab = año_fab
+        self.ano_fab = ano_fab
 
 class auto (vehiculo):
 
     descapotable = None
 
+    def __init__(self, patente,cant_ruedas,ano_fab, descapotable):
+
+        vehiculo.__init__(self, patente,cant_ruedas,ano_fab)
+
+        self.descapotable = descapotable
+
 class camioneta (vehiculo):
 
     cargaKg = None
+
+    def __init__(self,patente, cant_ruedas, ano_fab, cargaKg):
+
+        vehiculo.__init__(self, patente, cant_ruedas, ano_fab)
+
+        self.cargaKg = cargaKg
+
+
+
+
 
